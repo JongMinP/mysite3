a632f874132db7c3d91c83e25a4fbcb38e8ddeed
bit <bit@DESKTOP-A8LCUFN>
2018-03-28 21:27:41 +0900

    03 28 mysite commit

--- src/main/java/com/cafe24/mysite/repository/UserDao.java
+++ src/main/java/com/cafe24/mysite/repository/UserDao.java
@@ -3,6 +3,7 @@ package com.cafe24.mysite.repository;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
 
 import com.cafe24.mysite.vo.UserVo;
 
@@ -32,8 +33,20 @@ public class UserDao {
 	}
 
 	public UserVo get(UserVo vo) {
-
+		
 		return sqlSession.selectOne("user.getByEmailAndPassword", vo);
+		
+		
+		/* 이런식으로 사용 하려면 코드를 고쳐야 한다.*/
+//		StopWatch sw = new StopWatch();
+//		sw.start();
+//		
+//		UserVo result = sqlSession.selectOne("user.getByEmailAndPassword", vo);
+//		
+//		sw.stop();
+//		Long totalTime = sw.getTotalTimeMillis();
+//		
+//		return result;
 	}
 
 	public boolean insert(UserVo vo) {
